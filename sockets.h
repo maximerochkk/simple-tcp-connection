c0af863016df7862f3925b9ab6a9182b482f0159
maximerochkk <89849391+maximerochkk@users.noreply.github.com>
2022-04-14 17:51:43 +0200

    Update sockets.h

--- sockets.h
+++ sockets.h
@@ -1,30 +1,17 @@
-#include "sockets.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h> 
+#include <unistd.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <string.h> 
 
-int main(void) 
+#define PORT 2345
+#define IP "127.0.0.1"
+
+void fill_addr(struct sockaddr_in* addr, in_addr_t ip, uint16_t port, int family)
 {
-    int clientSocket;
-    struct sockaddr_in clientAddr;
-    char messageSent[100] = "Ce message provient du client !";
-
-    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        perror("socket()");
-        exit(EXIT_FAILURE);
-    }
-
-    fill_addr(&clientAddr, inet_addr(IP), htons(PORT), AF_INET);
-
-    if (connect(clientSocket, (struct sockaddr*) &clientAddr, sizeof(clientAddr)) == -1) {
-        perror("connect()");
-        exit(EXIT_FAILURE);
-    }
-
-    if (send(clientSocket, messageSent, sizeof(messageSent), 0) == -1) {
-        perror("send()");
-        exit(EXIT_FAILURE);
-    }
-
-    close(clientSocket);
-
-    return 0;
+    addr->sin_addr.s_addr = ip;
+    addr->sin_port = port;
+    addr->sin_family = family;
 }
